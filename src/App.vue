7c22ad371d3a900bb08912414f299f7627d7b3df
starhn87 <starhn87@gmail.com>
2022-02-22 22:19:45 +0900

    현재 진행 상황 공유

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,115 @@
 <template>
   <main class="flex-center">
     <section class="container">
-      <RadarChart />
+      <RadarChart :chartData="chartData" :options="options" />
+      <BarChart :options="options2" />
     </section>
   </main>
 </template>
 
 <script>
-import RadarChart from "./components/RadarChar.vue";
+import { surveyData } from "@/data";
+import BarChart from "./components/BarChart.vue";
+import RadarChart from "./components/RadarChart.vue";
 
 export default {
   name: "App",
   components: {
     RadarChart,
+    BarChart,
+  },
+
+  data() {
+    const {
+      userSurvey,
+      companySurvey: { lgSurvey },
+    } = surveyData;
+
+    const chartData = {
+      labels: [...Object.keys(userSurvey)],
+      datasets: [
+        {
+          backgroundColor: "rgba(255, 193, 74, 0.32)",
+          borderColor: "#FFD335",
+          borderWidth: 2,
+          borderRadius: 1,
+          pointBackgroundColor: "rgba(255,99,132,1)",
+          pointRadius: 0,
+          data: [...Object.values(lgSurvey)],
+        },
+        {
+          backgroundColor: "rgba(110, 60, 249, 0.32)",
+          borderColor: "#6E3CF9",
+          borderWidth: 2,
+          borderRadius: 1,
+          pointRadius: 0,
+          z: 1,
+          data: [...Object.values(userSurvey)],
+        },
+      ],
+    };
+
+    const image = new Image();
+    image.src = "./assets/cat.png";
+
+    const options = {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        display: false,
+      },
+      layout: {
+        padding: 20,
+      },
+      scale: {
+        pointLabels: {
+          fontColor: "black",
+          fontSize: 15,
+        },
+      },
+      plugins: [
+        {
+          id: "customImage",
+          beforeDraw: (chart) => {
+            if (image.complete) {
+              const ctx = chart.ctx;
+              const { top, left, width, height } = chart.chartArea;
+              const x = left + width - image.width;
+              const y = top + height - image.height;
+              ctx.drawImage(image, x, y);
+            } else {
+              image.onload = () => chart.draw();
+            }
+          },
+        },
+      ],
+    };
+
+    const options2 = {
+      legend: {
+        display: false,
+      },
+      annotation: {
+        annotations: [
+          {
+            type: "line",
+            mode: "horizontal",
+            scaleID: "y-axis-0",
+            borderWidth: 4,
+            label: {
+              enabled: false,
+              content: "Test label",
+            },
+          },
+        ],
+      },
+    };
+
+    return {
+      chartData,
+      options,
+      options2,
+    };
   },
 };
 </script>
---
+++ src/components/BarChart.vue
@@ -0,0 +1,47 @@
+<script>
+import { Bar } from "vue-chartjs";
+
+export default {
+  extends: Bar,
+  props: {
+    options: {
+      type: Object,
+      default: null,
+    },
+  },
+
+  handler(chart) {
+    console.log(chart);
+    chart.options.scales.x.position = "center";
+    chart.options.scales.y.position = "center";
+    chart.update();
+  },
+  mounted() {
+    this.renderChart(
+      {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ],
+        datasets: [
+          {
+            backgroundColor: "#f87979",
+            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
+          },
+        ],
+      },
+      this.options
+    );
+  },
+};
+</script>
