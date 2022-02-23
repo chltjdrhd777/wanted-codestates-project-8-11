e952be4f6a6038a205c6fb6ffeef06fef862c100
guno517 <60571418+guno517@users.noreply.github.com>
2022-02-23 12:08:09 +0900

    barChart 데이터 넣기

---
+++ src/components/BarChart.vue
@@ -0,0 +1,40 @@
+<script>
+import { HorizontalBar } from "vue-chartjs";
+
+export default {
+  extends: HorizontalBar,
+  props: {
+    surveyData: Object,
+  },
+  mounted() {
+    let obj = [Object.values(this.surveyData.userSurvey)];
+    let negativeObj = obj[0].map(i => i*-1);
+    this.renderChart(
+      {
+        labels: [...Object.keys(this.surveyData.userSurvey)], // 테이블 만들어지면 지울 것
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [...negativeObj]
+          },
+          {
+            label: "Data two",
+            backgroundColor: "#f87979",
+            data: [...Object.values(this.surveyData.companySurvey.samsungSurvey)]
+          }
+        ]
+      },
+      { responsive: true, maintainAspectRatio: false,
+      legend: {
+        display: false,
+      title: {
+        display: true,
+        text: 'Chart.js Horizontal Bar Chart'
+      }
+    }
+      }
+    );
+  }
+};
+</script>
