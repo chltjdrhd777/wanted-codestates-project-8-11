e2e86ad3307cfbac8067b64f1bfd26204bc18b71
hatoba29 <hatoba29@naver.com>
2022-02-23 14:55:30 +0900

    FEAT: 펜타곤 그래프 수정
    - 바깥쪽 텍스트 정렬
    - 각 꼭짓점에 원형 포인트 추가
    - Hover시 나타나는 인터랙션 제거

--- src/App.vue
+++ src/App.vue
@@ -27,11 +27,13 @@ export default {
       companySurvey: { samsungSurvey },
     } = surveyData;
     const chartData = {
-      labels: [["적극적인", "Aggressive"],
-        ["자신있는", "Confident"],
-        ["책임있는", "Responsible"],
-        ["개인주의", "Indivisual"],
-        ["수평적인", "Horizontal"],],
+      labels: [
+        ["적극적인", "Aggressive", ""],
+        ["  자신있는", "Confident", ""],
+        ["", "    책임있는", "Responsible"],
+        ["", "개인주의  ", "Indivisual"],
+        ["수평적인   ", "Horizontal", ""],
+      ],
       datasets: [
         {
           backgroundColor: "rgba(255, 193, 74, 0.32)",
@@ -40,6 +42,8 @@ export default {
           borderRadius: 1,
           pointBackgroundColor: "rgba(255,99,132,1)",
           pointRadius: 0,
+          pointHitRadius: 0,
+          order: 1,
           data: [...Object.values(samsungSurvey)],
         },
         {
@@ -48,8 +52,32 @@ export default {
           borderWidth: 2,
           borderRadius: 1,
           pointRadius: 0,
-          z: 1,
+          pointHitRadius: 0,
+          order: 2,
           data: [...Object.values(userSurvey)],
+        },
+        {
+          backgroundColor: "rgba(244, 244, 244, 0.32)",
+          borderColor: "#B2B2B2",
+          borderWidth: 1,
+          pointBackgroundColor: [
+            "rgba(237, 168, 154, 0.7)",
+            "rgba(182, 197, 248, 0.7)",
+            "rgba(164, 214, 227, 0.7)",
+            "rgba(238, 184, 207, 0.7)",
+            "rgba(183, 220, 171, 0.7)",
+          ],
+          pointBorderColor: [
+            "#EDA89A",
+            "#B6C5F8",
+            "#A4D6E3",
+            "#EEB8CF",
+            "#B7DCAB",
+          ],
+          pointRadius: 16,
+          pointHitRadius: -16,
+          order: 0,
+          data: [10, 10, 10, 10, 10],
         },
       ],
     };
@@ -65,14 +93,19 @@ export default {
       },
       scale: {
         pointLabels: {
+          fontFamily: "Noto Sans",
+          fontStyle: "bold",
           fontColor: "black",
-          fontSize: 15,
+          fontSize: 14,
         },
         ticks: {
           max: 10,
           min: 0,
           stepSize: 2.5,
           display: false,
+        },
+        gridLines: {
+          borderDash: [5, 5],
         },
       },
     };
@@ -127,10 +160,8 @@ main {
   border-radius: 160px;
   overflow: hidden;
   position: absolute;
-  left: 42.22%;
-  right: 42.78%;
-  top: 43.57%;
-  bottom: 42.26%;
+  left: 43%;
+  top: 49%;
   background-color: white;
   object-fit: cover;
 }
