f14fad53a2ea866a6a4b5d0dfc86d0bcb8580536
guno517 <guno5170@gmail.com>
2022-02-23 15:32:49 +0900

    Design: 테이블 UI로 변경

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -8,34 +8,66 @@ export default {
   },
   mounted() {
     // negativeObj: 양 옆으로 그래프가 표시되도록 userSurvey는 음수 값으로 변경하였습니다.
-    let obj = [Object.values(this.surveyData.userSurvey)];
-    let negativeObj = obj[0].map(i => i*-1);
+    let userObj = [Object.values(this.surveyData.userSurvey)]; 
+    let companyObj = [Object.values(this.surveyData.companySurvey.samsungSurvey)]; 
+    let negativeUserObj = userObj[0].map((i)=> i>=5 ? i*(-1) : 10 - i );
+    let negativeCompanyObj = companyObj[0].map((i)=> i>=5 ? i*(-1) : 10 - i );
     this.renderChart(
       {
-        labels: [...Object.keys(this.surveyData.userSurvey)], // 테이블 만들어지면 지울 것
+        labels: ["","","","",""], // 테이블 만들어지면 지울 것
         datasets: [
           {
             label: "Data One",
-            backgroundColor: "#f87979",
-            data: [...negativeObj]
+            backgroundColor: "#6E3CF9",
+            data: [...negativeUserObj],
           },
           {
             label: "Data two",
-            backgroundColor: "#f87979",
-            data: [...Object.values(this.surveyData.companySurvey.samsungSurvey)]
+            backgroundColor: "#FFC24A",
+            data: [...negativeCompanyObj],
           },
         ],
       },
       { 
         responsive: true,
         maintainAspectRatio: false,
-        legend: {
-          display: false,
-          title: {
-            display: true,
-            text: 'Chart.js Horizontal Bar Chart'
+        legend: { 
+          display: false 
+        },
+        tooltips:{
+          callbacks: {
+            label:function(tooltipItem){
+              return tooltipItem.yLabel;
+            }
           }
-        }
+        },
+        scales: {
+          yAxes: [
+            {
+              barPercentage: 0.5,
+              gridLines: {
+                display: false,
+              },
+              ticks: {
+                fontSize: 20,
+                beginAtZero: true,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+              ticks: {
+                display: false,
+                min: -10,
+                max: 10,
+                stepSize: 20,
+              },
+            },
+          ],
+        },
       }
     );
   }
