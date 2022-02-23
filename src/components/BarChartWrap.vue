4d7672ee7b5099bc45a111617f12c57aa878bfda
chloe41297 <79011228+chloe41297@users.noreply.github.com>
2022-02-23 15:50:51 +0900

    feat : table area 코드 가독성 높이기 및 디테일 추가

--- src/components/BarChartWrap.vue
+++ src/components/BarChartWrap.vue
@@ -5,42 +5,36 @@
       <h3>나의 결과는?</h3>
     </div>
     <table>
-      <tr>
-        <td>{{ data.aggressive }}/{{ MAX_SCORE }}</td>
-        <td style="[color : data.aggressive > 5 ? green : black ]">적극성</td>
-        <td rowspan="5" class="chart">
-          <BarChart width="80px" height="230px" />
+      <tr v-for="(list, i) in userObj" v-bind:key="list">
+        <td
+          :style="[data[list] >= 5 ? { color: 'green' } : { color: 'black' }]"
+        >
+          {{ data[list] }}/{{ MAX_SCORE }}
         </td>
-        <td>수동성</td>
-        <td>{{ MAX_SCORE - data.aggressive }}/{{ MAX_SCORE }}</td>
-      </tr>
-      <tr>
-        <td>{{ data.confident }}/{{ MAX_SCORE }}</td>
-        <td>자신감</td>
-        <!-- <td></td> -->
-        <td>신중함</td>
-        <td>{{ MAX_SCORE - data.confident }}/{{ MAX_SCORE }}</td>
-      </tr>
-      <tr>
-        <td>{{ data.responsible }}/{{ MAX_SCORE }}</td>
-        <td>책임감</td>
-        <!-- <td></td> -->
-        <td>무심함</td>
-        <td>{{ MAX_SCORE - data.responsible }}/{{ MAX_SCORE }}</td>
-      </tr>
-      <tr>
-        <td>{{ data.indivisual }}/{{ MAX_SCORE }}</td>
-        <td>개인성향</td>
-        <!-- <td></td> -->
-        <td>조직성향</td>
-        <td>{{ MAX_SCORE - data.indivisual }}/{{ MAX_SCORE }}</td>
-      </tr>
-      <tr>
-        <td>{{ data.horizontal }}/{{ MAX_SCORE }}</td>
-        <td>수평사고</td>
-        <!-- <td></td> -->
-        <td>위계사고</td>
-        <td>{{ MAX_SCORE - data.horizontal }}/{{ MAX_SCORE }}</td>
+        <td
+          :style="[
+            data[list] >= 5
+              ? { color: 'green', fontWeight: 700, fontSize: '12px' }
+              : { color: 'black' },
+          ]"
+        >
+          {{ personalityArray[i][0] }}
+        </td>
+        <td v-if="i === 0" rowspan="5" class="chart">
+          <BarChart width="80" height="230" />
+        </td>
+        <td
+          :style="[
+            data[list] < 5
+              ? { color: 'green', fontWeight: 700, fontSize: '12px' }
+              : { color: 'black ' },
+          ]"
+        >
+          {{ personalityArray[i][1] }}
+        </td>
+        <td :style="[data[list] < 5 ? { color: 'green' } : { color: 'black' }]">
+          {{ MAX_SCORE - data[list] }}/{{ MAX_SCORE }}
+        </td>
       </tr>
     </table>
   </section>
@@ -51,11 +45,17 @@ import { surveyData } from "@/data";
 
 const { userSurvey } = surveyData;
 const MAX_SCORE = 10;
+const userObj = [...Object.keys(userSurvey)];
+const personalityArray = [
+  ["적극성", "수동성"],
+  ["자신감", "신중함"],
+  ["책임감", "무심함"],
+  ["개인 성향", "조직 성향"],
+  ["수평사고", "위계사고"],
+];
 
 export default {
-  mounted() {
-    console.log("bar", this.options);
-  },
+  mounted() {},
   components: {
     BarChart,
   },
@@ -63,6 +63,8 @@ export default {
     return {
       data: userSurvey,
       MAX_SCORE,
+      userObj: userObj,
+      personalityArray: personalityArray,
     };
   },
 };
@@ -85,9 +87,10 @@ h3 {
   margin-bottom: 10px;
 }
 table {
-  width: 100%;
+  width: calc(100% - 40px);
   font-size: 10px;
   text-align: center;
+  margin: 0 20px;
 }
 .chart {
   /* display: flex;
